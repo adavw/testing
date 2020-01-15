cb55dcea6acdb99bcf239d710fb12691948041b7
Adam Weeks <logicallow@gmail.com>
2020-01-15 12:27:54 -0800

    unknown changes

--- rand20.py
+++ rand20.py
@@ -1,7 +1,12 @@
 import secrets
 
 b = 0
+x = []
 
-while b < 20:
-    print(secrets.randbits(8))
-    b = b + 1
+def makerand():
+    b = 0
+    x = list(range(0, 20))
+    while (b < 20):
+        x[b] = (secrets.randbits(8))
+        b = b + 1
+    return x
